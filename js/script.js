0193a2fe0ed5c2bffce8792e6807cb833aa667c4
PIDLISNYiDMYTRo <pidlisnyifrontjob@gmail.com>
2023-07-16 13:55:22 +0300

    mobile-responsive ver.03

--- js/script.js
+++ js/script.js
@@ -55,10 +55,16 @@
 
 
 
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+function setContainerHeight() {
+	var windowHeight = window.innerHeight;
+	document.querySelector('.wrapper').style.height = windowHeight + 'px';
+}
+
+// Call the function initially to set the container height
+setContainerHeight();
+
+// Call the function whenever the window is resized
+window.addEventListener('resize', setContainerHeight);
 
 
 
